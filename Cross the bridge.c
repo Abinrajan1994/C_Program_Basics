21eafda00a44ff5eaa6569ca0db4914cc67d1c54
Abinrajan1994 <148305169+Abinrajan1994@users.noreply.github.com>
2025-11-03 14:08:52 +0530

    Create Cross the bridge.c

---
+++ Cross the bridge.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int x,y,z,r;
+    // Assume input 2 3 4
+    scanf("%d%d%d",&x,&y,&z); // x=2, y=3, z=4
+
+    // Write your logic here
+    // Calculate the sum of x, y, and z
+    r = x + y + z;
+
+    printf("%d",r); // Prints 9
+
+    return 0;
+
+}
